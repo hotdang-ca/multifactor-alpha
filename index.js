e05bc7a6d1d0de7d172f1ad7f29122bd3f21ab3d
James Robert Perih <james@hotdang.ca>
2019-03-25 12:44:47 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,79 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const uuidv1 = require('uuid/v1');
+
+const PORT = 3002;
+const DEFAULT_NUM_AUTHS = 3;
+
+const authDict = {};
+
+app.get('/', (req, res) => {
+  // generate a new uuid
+  res.sendFile(`${__dirname}/html/index.html`);
+});
+
+app.get('/authorize/:uuid', (req, res) => {
+  res.sendFile(`${__dirname}/html/uuid_auth.html`);
+});
+
+io.on('connection', (socket) => {
+  socket.on('require-auth', () => {
+    uuid = uuidv1();
+    numAuthorizations = 0;
+
+    authDict[uuid] = numAuthorizations;
+
+    console.log(`new auth for ${uuid}`);
+
+    console.log('auths: ', authDict);
+
+    const payload = {
+      uuid,
+      required: DEFAULT_NUM_AUTHS,
+    };
+
+    socket.emit('got-uuid', JSON.stringify(payload));
+  });
+
+  socket.on('give-auth', (payload) => {
+    const payloadObject = JSON.parse(payload);
+    if (!payloadObject) {
+      console.log('No payload Object');
+      return;
+    }
+
+    if (!payloadObject.uuid) {
+      console.log('No UUID with payload');
+      return;
+    }
+
+    let dictEntry = authDict[payloadObject.uuid];
+    if (typeof dictEntry === 'undefined') {
+      console.log(`Received an authorization we aren't waiting for: ${payloadObject.uuid}`);
+      return;
+    }
+
+    console.log('Got a matching authorization!');
+    dictEntry += 1;
+    authDict[payloadObject.uuid] = dictEntry;
+
+    console.log('AuthDict', authDict);
+
+    const msg = {
+      message: `Received an authorization for ${payloadObject.uuid}`,
+      uuid: payloadObject.uuid,
+      count: dictEntry,
+      isMetRequirement: dictEntry >= DEFAULT_NUM_AUTHS, // or, tenant-specific
+    };
+
+    console.log('emitting message: ', msg);
+
+    // send to everyone
+    io.emit('auth-msg', JSON.stringify(msg));
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
