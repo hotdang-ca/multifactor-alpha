3f4b16ae84260504753cdf8fa6a18e9f68fc0f4b
James Perih <james@hotdang.ca>
2020-06-20 16:10:53 -0600

    Add some chat features for a different project.

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const config = require('dotenv').config();
 
+const INCOMING = 'incoming';
+const OUTGOING = 'outgoing';
+
 // setup mailgun
 const mailgun = require("mailgun-js");
 const MG_DOMAIN = process.env.MAILGUN_DOMAIN;
@@ -37,7 +40,6 @@ const twilio = new Twilio(twilioAccountSid, twilioAuthToken);
 
 const PORT = process.env.PORT || 3002;
 const redirects = [];
-
 const contacts = {};
 // {
 //   authkey: [
@@ -68,6 +70,164 @@ function cleanNumber(number) {
   console.log('number is now ', number);
   return number;
 }
+
+const _chats = [
+  {
+    userId: 1,
+    body: 'Hey dude',
+    direction: INCOMING,
+    dateTime: new Date(
+      2020, 6, 10, 12, 30, 43, 0,
+    ),
+  },
+  {
+    userId: 1,
+    body: 'Hey man',
+    direction: OUTGOING,
+    dateTime: new Date(
+      2020, 6, 10, 12, 31, 43, 0,
+    ),
+  }
+];
+
+const _chatUsers = [
+  {
+    userId: 1,
+    name: 'James Perih',
+    email: 'james@hotdang.ca',
+    sms: '+13065809501',
+  }
+];
+
+// UNRELATED. This is the messaging endpoint i might work on for Villains
+app.post('/api/messaging/incoming-sms', (req, res) => {
+  const smsBody = req.body.Body || req.body.body;
+  const from = req.body.From || req.body.from;
+
+  const user = _chatUsers.find((c) => c.sms === from);
+  if (!user) {
+    console.log('no user found.');
+    return res.status(200).json({ message: 'no such user. But we\'ll store them message.'});
+  }
+
+  const chatItem = {
+    userId: user.userId,
+    direction: INCOMING,
+    body: smsBody,
+    dateTime: new Date(),
+  }
+
+  _chats.push(chatItem);
+
+  io.emit('next-message', JSON.stringify(chatItem));
+  return res.status(200).json({ status: 'ok'});
+  // tell someone to update
+});
+
+app.post('/api/messaging/incoming-email', (req, res) => {
+
+});
+
+// UNRELATED. This is the messaging endpoint i might work on for Villains
+app.post('/api/messaging/send-sms', (req, res) => {
+  const { userId, content } = req.body;
+  console.log(req.body);
+
+  const user = _chatUsers.find((c) => c.userId === userId);
+  if (!user) {
+    return res.status(404).json({ error: 'not found'});
+  }
+
+  // update history
+  _chats.push({
+    userId,
+    body: content,
+    direction: OUTGOING,
+    dateTime: new Date(),
+  });
+
+  const messageBody = {
+    from: twilioFrom,
+    to: user.sms,
+    body: `FROM VILLAINS:\n${content}`,
+  };
+  
+  twilio.messages.create(messageBody, (err, result) => {
+    if (err) {
+      console.log('Twilio error:', err);
+      return;
+    }
+    console.log(`Twilio result: ${result.sid}`);
+  });
+  
+  // send back new history
+  // TODO: make this accumulative maybe
+  res.status(200).json({
+    status: 'ok',
+  });
+
+});
+
+app.get('/api/messaging/users', (req, res) => {
+  return res.status(200).json(_chatUsers);
+});
+
+app.post('/api/messaging/users', (req, res) => {
+  const { name, sms, email } = req.body;
+  const user = {
+    name,
+    sms,
+    email,
+    userId: _chatUsers[_chatUsers.length -1].userId + 1,
+  }
+
+  _chatUsers.push(user);
+  io.emit('users-changed', JSON.stringify(_chatUsers));
+
+  return res.status(200).json(_chatUsers);
+});
+
+app.post('/api/messaging/send-email', (req, res) => {
+  const { userId, content } = req.body;
+
+  const user = _chatUsers.find((u) => u.userId === parseInt(userId));
+  if (!user) {
+    return res.status(404).json({ error: 'no such user.'});
+  }
+
+  const data = {
+    from: `Villains Strength And Conditioning <${MG_FROM}>`,
+    to: user.email,
+    subject: 'Ongoing chat',
+    text: content,
+  };
+
+  mg.messages().send(data, function (error, body) {
+    return res.status(200).json({ status: 'ok'});
+  });
+});
+
+app.get('/api/messaging/get-history/:userId', (req, res) => {
+  const { userId } = req.params;
+  console.log('fetch history for userId ', userId);
+  console.log('all chats', _chats);
+
+  const history = _chats.filter((c) => c.userId === parseInt(userId));
+  const user = _chatUsers.find((c) => c.userId === parseInt(userId));
+  
+  const payload = {
+    history,
+    user,
+  };
+
+  io.emit('next-messages', JSON.stringify(payload));
+  return res.status(200).json(payload || {});
+});
+
+// UNRELATED. This is the messaging interface for Villains
+app.get('/chat', (req, res) => {
+  res.sendFile(`${__dirname}/html/chat.html`);
+});
 
 // TODO: set up webhook to listen to incoming sms
 app.post('/api/webhooks/incoming-sms', (req, res) => {
@@ -238,6 +398,21 @@ io.on('connection', (socket) => {
 
     // send to everyone
     io.emit('auth-msg', JSON.stringify(msg));
+  });
+
+  socket.on('get-next-message', (payload) => {
+    const payloadObject = JSON.parse(payload);
+    if (!payloadObject) {
+      console.log('No payload Object');
+      return;
+    }
+
+    const { userId } = payloadObject;
+    const user = _chatUsers.find((u) => u.userId === parseInt(userId));
+    const userChats = _chats.filter((u) => u.userId === user.userId);
+    const lastChat = userChats[userChats.length -1];
+
+    io.emit('next-message', JSON.stringify(lastChat));
   });
 });
 
